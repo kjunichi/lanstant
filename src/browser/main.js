fe52371fa7bb36db84efbc7ed1b739f6c1478ab5
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-01-17 20:46:12 +0900

    update loading scene

--- src/browser/main.js
+++ src/browser/main.js
@@ -61,7 +61,7 @@ app.on('ready', function() {
     }
   }
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1024, height: 900});
 
   // and load the index.html of the app.
   var targetPath = path.resolve(__dirname, '..', '..', 'static', 'index.html');
